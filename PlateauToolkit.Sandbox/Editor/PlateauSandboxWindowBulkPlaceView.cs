022478fa2fc3d3c949e45bb85be7edc588c7bfca
yhikishima <lallapalooza1120@gmail.com>
2024-10-12 08:24:09 +0900

    fix:csvテンプレート作成を共通化

--- PlateauToolkit.Sandbox/Editor/PlateauSandboxWindowBulkPlaceView.cs
+++ PlateauToolkit.Sandbox/Editor/PlateauSandboxWindowBulkPlaceView.cs
@@ -154,38 +154,7 @@ namespace PlateauToolkit.Sandbox.Editor
             {
                 return;
             }
-
-            var templateData = new List<PlateauSandboxBulkPlaceDataBase>();
-            var data = new PlateauSandboxBulkPlaceCsvData(0, new List<string>()
-            {
-                "34.9873", "135.7596", "14.23", "イチョウ"
-            }, new List<string>()
-            {
-                "緯度", "経度", "高さ", "アセット種別"
-            });
-            templateData.Add(data);
-
-            data = new PlateauSandboxBulkPlaceCsvData(1, new List<string>()
-            {
-                "34.98742", "135.7596", "16.3", "ユリノキ"
-            }, new List<string>()
-            {
-                "緯度", "経度", "高さ", "アセット種別"
-            });
-            templateData.Add(data);
-
-            bool saveSuccess = new PlateauSandboxFileCsvParser().Save(filePath, templateData);
-            if (!saveSuccess)
-            {
-                return;
-            }
-
-            string directoryName = Path.GetDirectoryName(filePath);
-            if (!string.IsNullOrEmpty(directoryName))
-            {
-                System.Diagnostics.Process.Start(directoryName);
-            }
-            Debug.Log($"CSVテンプレートを保存しました: {filePath}");
+            PlateauSandboxCsvTemplate.Create(filePath);
         }
 
         void OnGUIFieldType(PlateauSandboxContext context)
