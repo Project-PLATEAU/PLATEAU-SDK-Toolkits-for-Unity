f71c709a7ecfc3c460ace5d14efa216a1711cec3
Linoal <1321932+linoal@users.noreply.github.com>
2025-02-21 17:35:17 +0900

    バージョン情報更新。メニューバーからデプロイ。

--- PlateauToolkit.Utilities/Editor/PlateauUtilitiesPaths.cs
+++ PlateauToolkit.Utilities/Editor/PlateauUtilitiesPaths.cs
@@ -2,7 +2,8 @@ namespace PlateauToolkit.Utilities.Editor
 {
     public class PlateauUtilitiesPaths
     {
-        const string k_SpritesFolder = "Packages/com.synesthesias.plateau-unity-toolkit/PlateauToolkit.Utilities/Editor/Sprites";
+        public const string k_ToolkitBasePath = "Packages/com.synesthesias.plateau-unity-toolkit";
+        const string k_SpritesFolder = k_ToolkitBasePath + "/PlateauToolkit.Utilities/Editor/Sprites";
 
         public static readonly string k_VertexFlattenIcon = $"{k_SpritesFolder}/PlateauToolkitUtilities_Selector.png";
     }
