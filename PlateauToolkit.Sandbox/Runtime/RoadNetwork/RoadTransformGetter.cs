d6a7edc021df648d752f8722e062d8329ccf3e1f
sevendev <taka@7jigen.net>
2025-12-19 14:57:58 +0900

    Refactoring

--- PlateauToolkit.Sandbox/Runtime/RoadNetwork/RoadTransformGetter.cs
+++ PlateauToolkit.Sandbox/Runtime/RoadNetwork/RoadTransformGetter.cs
@@ -8,17 +8,14 @@ namespace PlateauToolkit.Sandbox.RoadNetwork
     {
         const string k_ROAD_PREFIX = "Road-"; //PLATEAUReproducedRoad名のPrefix
 
-        Transform m_ReproducedRoadRoot;
+        readonly Transform m_ReproducedRoadRoot;
         public Transform ReproducedRoad => m_ReproducedRoadRoot;
 
         public RoadTransformGetter()
         {
             // 最初に見つかったPLATEAUReproducedRoadの親を取得する(ReproducedRoadのParentはシーンに1つと想定）
             PLATEAUReproducedRoad found = Object.FindFirstObjectByType<PLATEAUReproducedRoad>();
-            if (found != null)
-            {
-                m_ReproducedRoadRoot = found.transform.parent;
-            }
+            m_ReproducedRoadRoot = found?.transform.parent;
         }
 
         public Transform GetRoadTransform(string gmlId)
@@ -29,15 +26,8 @@ namespace PlateauToolkit.Sandbox.RoadNetwork
             }
 
             string roadName = k_ROAD_PREFIX + gmlId;
-            Transform roadTransform = m_ReproducedRoadRoot.Find(roadName);
-
-            if (roadTransform == null)
-            {
-                //取得できないケースもあるが無視
-                return null;
-            }
-
-            return roadTransform;
+            // 取得できないケースもあるが無視
+            return m_ReproducedRoadRoot.Find(roadName);
         }
     }
 }
